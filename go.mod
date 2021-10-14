300e853e9916d5b60380a59fbb9701f333e6a668
Razvan Mandu <rmandu@mcclatchyservices.com>
2021-10-14 11:18:17 +0300

    test swagger

--- go.mod
+++ go.mod
@@ -1,23 +1,40 @@
 module github.com/jcamargoendava/pokemonwiki
 
+
 go 1.16
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
 	github.com/gin-gonic/gin v1.7.2
+	github.com/go-openapi/runtime v0.20.0 // indirect
 	github.com/go-playground/validator/v10 v10.7.0 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/go-swagger/go-swagger v0.28.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/snappy v0.0.3 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/json-iterator/go v1.1.11 // indirect
-	github.com/kamva/mgm/v3 v3.3.1
+	github.com/kr/text v0.2.0 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/mtslzr/pokeapi-go v1.4.0
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/swaggo/files v0.0.0-20210815190702-a29dd2bc99b2 // indirect
+	github.com/swaggo/gin-swagger v1.3.2 // indirect
+	github.com/swaggo/swag v1.7.3 // indirect
 	github.com/ugorji/go v1.2.6 // indirect
-	go.mongodb.org/mongo-driver v1.7.0
-	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
-	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
-	golang.org/x/text v0.3.6 // indirect
+	go.mongodb.org/mongo-driver v1.7.3
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/net v0.0.0-20211013171255-e13a2654a71e // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20211013075003-97ac67df715c // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- routes/routes.go
+++ routes/routes.go
@@ -1,8 +1,56 @@
+// @title Swagger Example API
+// @version 1.0
+// @description This is a sample server celler server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8081
+// @BasePath /api/v1
+// @query.collection.format multi
+
+// @securityDefinitions.basic BasicAuth
+
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+
+// @securitydefinitions.oauth2.application OAuth2Application
+// @tokenUrl https://example.com/oauth/token
+// @scope.write Grants write access
+// @scope.admin Grants read and write access to administrative information
+
+// @securitydefinitions.oauth2.implicit OAuth2Implicit
+// @authorizationurl https://example.com/oauth/authorize
+// @scope.write Grants write access
+// @scope.admin Grants read and write access to administrative information
+
+// @securitydefinitions.oauth2.password OAuth2Password
+// @tokenUrl https://example.com/oauth/token
+// @scope.read Grants read access
+// @scope.write Grants write access
+// @scope.admin Grants read and write access to administrative information
+
+// @securitydefinitions.oauth2.accessCode OAuth2AccessCode
+// @tokenUrl https://example.com/oauth/token
+// @authorizationurl https://example.com/oauth/authorize
+// @scope.admin Grants read and write access to administrative information
+
+// @x-extension-openapi {"example": "value on a json format"}
 package routes
 
 import (
 	"context"
 	"net/http"
+
+	swaggerFiles "github.com/swaggo/files"     // swagger embed files
+	ginSwagger "github.com/swaggo/gin-swagger" // gin-swagger middleware
+	// "github.com/jcamargoendava/pokemonwiki/docs"
 
 	"github.com/gin-gonic/gin"
 	pokemonController "github.com/jcamargoendava/pokemonwiki/controllers"
@@ -19,22 +67,24 @@ func ApiMiddleware(ctx context.Context) gin.HandlerFunc {
 func StartGin(ctx context.Context) {
 	router := gin.Default()
 	router.Use(ApiMiddleware(ctx))
-	api := router.Group("/api")
-	{
-		api.GET("are-you-alive", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{"status": "running"})
-		})
-		api.GET("/pokemons", pokemonController.GetPokemons)
-		api.GET("/pokemon/:name", pokemonController.GetPokemon)
-		api.POST("/pokemon", pokemonController.CreatePokemon)
-		api.PUT("/pokemon/:id", pokemonController.UpdatePokemon)
-		api.DELETE("/pokemon/:id", pokemonController.DeletePokemon)
+	// api := router.Group("/api")
 
-		api.GET("/master/:id", pokemonController.GetMaster)
-		api.POST("/master", pokemonController.CreateMaster)
-		api.PUT("/master/:id", pokemonController.UpdateMaster)
-		api.DELETE("/master/:id", pokemonController.DeleteMaster)
-	}
+	router.GET("are-you-alive", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"status": "running"})
+	})
+	router.GET("/pokemons", pokemonController.GetPokemons)
+	router.GET("/pokemon/:name", pokemonController.GetPokemon)
+	router.POST("/pokemon", pokemonController.CreatePokemon)
+	router.PUT("/pokemon/:id", pokemonController.UpdatePokemon)
+	router.DELETE("/pokemon/:id", pokemonController.DeletePokemon)
+
+	router.GET("/master/:id", pokemonController.GetMaster)
+	router.POST("/master", pokemonController.CreateMaster)
+	router.PUT("/master/:id", pokemonController.UpdateMaster)
+	router.DELETE("/master/:id", pokemonController.DeleteMaster)
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
 	router.NoRoute(func(c *gin.Context) {
 		c.AbortWithStatus(http.StatusNotFound)
 	})
